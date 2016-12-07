7dc0e973d39c1f459e6b7bd21252794abfca2211
yaqing-xie <yx2316@columbia.edu>
2016-12-07 04:40:23 -0500

    zs part

--- lib/text_recognition.py
+++ lib/text_recognition.py
@@ -5,22 +5,29 @@
 import sys
 import cv2
 import numpy as np
+import pandas as pd
 from PIL import Image, ImageEnhance, ImageFilter
 from pytesseract import image_to_string
 
 
+
+wd = "C:/Users/Qing/Documents/GitHub/Fall2016-proj5-grp9"
+test_id = '2014114'
+
+
+
 # To count number of charaters and output one string
 def ImageSting_count(img_path):
-	# Open image
-	im = Image.open(img_path)
-	ip = img_path
-	# Image enhancers
-	enhancer = ImageEnhance.Contrast(im)
-	im = enhancer.enhance(1)
-	# Extract string
-	text = pytesseract.image_to_string(im)
-	count = len(text)
-	return count, text, ip
+    # Open image
+    im = Image.open(img_path)
+    ip = img_path
+    # Image enhancers
+    enhancer = ImageEnhance.Contrast(im)
+    im = enhancer.enhance(1)
+    # Extract string
+    text = pytesseract.image_to_string(im)
+    count = len(text)
+    return count, text, ip
 
 
 def preprocess(gray):
@@ -38,10 +45,10 @@ def preprocess(gray):
     dilation2 = cv2.dilate(erosion, element2, iterations = 3)
 
     # save intermediate pictures 
-    cv2.imwrite("binary.png", binary)
-    cv2.imwrite("dilation.png", dilation)
-    cv2.imwrite("erosion.png", erosion)
-    cv2.imwrite("dilation2.png", dilation2)
+    #cv2.imwrite(wd+"/output/text_detection/"+test_id+"_binary.png", binary)
+    #cv2.imwrite(wd+"/output/text_detection/"+test_id+"_dilation.png", dilation)
+    #cv2.imwrite(wd+"/output/text_detection/"+test_id+"_erosion.png", erosion) 
+    #cv2.imwrite(wd+"/output/text_detection/"+test_id+"_dilation2.png", dilation2)
 
     return dilation2
 
@@ -49,7 +56,7 @@ def findTextRegion(img):
     region = []
 
     # Find boxes
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    img2, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     for i in range(len(contours)):
         cnt = contours[i]
@@ -62,11 +69,11 @@ def findTextRegion(img):
         epsilon = 0.001 * cv2.arcLength(cnt, True)
         approx = cv2.approxPolyDP(cnt, epsilon, True)
         rect = cv2.minAreaRect(cnt)
-        print "rect is: "
-        print rect
+        print("rect is: ")
+        print(rect)
 
         # four corners
-        box = cv2.cv.BoxPoints(rect)
+        box = cv2.boxPoints(rect)
         box = np.int0(box)
 
         # calculate height and width
@@ -80,40 +87,23 @@ def findTextRegion(img):
 
     return region
 
-def detect(img):
-    # Gray
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Shape
-    dilation = preprocess(gray)
-
-    # find text regions
-    region = findTextRegion(dilation)
-
-    # Draw lines to form boxes
-    for box in region:
-        cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
-
-    cv2.namedWindow("img", cv2.WINDOW_NORMAL)
-    cv2.imshow("img", img)
-
-    cv2.imwrite("contours.png", img)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 def get_num_pixels(img):
-    width, height = Image.open(open(filepath)).size
+    width, height = Image.open(img).size
     return width*height
 
 def percentage_text(img):
-	region = []
-	barea = []
-	# Total amount of pixels
-	total_px = get_num_pixels(img)
+    region = []
+    barea = []
+    # Total amount of pixels
+    total_px = get_num_pixels(img)
+    # Gray
+    gray = cv2.imread(img,0)
 
+    # Shape
+    dilation = preprocess(gray)
     # Find boxes
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    img2, contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     for i in range(len(contours)):
         cnt = contours[i]
@@ -130,7 +120,7 @@ def percentage_text(img):
         #print rect
 
         # four corners
-        box = cv2.cv.BoxPoints(rect)
+        box = cv2.boxPoints(rect)
         box = np.int0(box)
 
         # calculate height and width
@@ -143,9 +133,35 @@ def percentage_text(img):
 
     total_area = sum(barea)
 
+    a = pd.DataFrame([float(total_area)/float(total_px)])
 
-    return float(total_area)/float(total_px)
+    a.to_csv('C:/Users/Qing/Documents/GitHub/Fall2016-proj5-grp9/output/test_detection'+"\\"+test_id+"_text.csv")
 
+def detect(img):
+    # Gray
+    gray = cv2.imread(img,0)
 
+    # Shape
+    dilation = preprocess(gray)
 
+    # find text regions
+    region = findTextRegion(dilation)
+    
+    
+    d = cv2.imread(img)
+
+    # Draw lines to form boxes
+    for box in region:
+        cv2.drawContours(d, [box], 0, (0, 255, 0), 2)
+
+    #cv2.namedWindow("img", cv2.WINDOW_NORMAL)
+    #cv2.imshow("img", d)
+
+    cv2.imwrite('C:/Users/Qing/Documents/GitHub/Fall2016-proj5-grp9/output/test_detection'+"\\"+test_id+"_contours.png", d)
+
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+detect(img = wd+"/data/test_images/"+test_id+".jpg")
+percentage_text(img = wd+"/data/test_images/"+test_id+".jpg")
 
