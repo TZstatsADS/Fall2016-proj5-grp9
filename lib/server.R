724e244da0e60ee48766b26891b0c7dd1968a951
yaqing-xie <yx2316@columbia.edu>
2016-12-07 01:39:14 -0500

    part xieyaqing

--- lib/server.R
+++ lib/server.R
@@ -2,7 +2,11 @@ library(RColorBrewer)
 library(shiny)
 library(ggplot2)
 library(plotly)
-s_gross_genre<-read.csv("/Users/Cristina/Desktop/16 Fall/5243 ADS/Project 5/data/s_gross_genre.csv")
+
+wd <- "/Users/YaqingXie/Desktop/3-Applied Data Science/Fall2016-proj5-grp9"
+setwd(wd)
+
+s_gross_genre<-read.csv("./data/s_gross_genre.csv")
 s_gross_genre<-as.data.frame(s_gross_genre)
 
 shinyServer(function(input, output) {
@@ -23,8 +27,180 @@ shinyServer(function(input, output) {
   geom_bar(colour="black",stat="identity")
   ggplotly()
   })
+
+  x_values <- reactiveValues(x_tops = NULL, x_similars = NULL, x_sim_genre = NULL,
+                             x_progress = NULL, x_predict_table = NULL)
   
+  output$x_image <- renderImage({
+    x_fileName <- input$x_file$name
+    x_fileImage <- paste0(wd , '/data/test_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=260))
+  }, deleteFile = FALSE)
+  
+  observeEvent(input$x_start_analyze, {
+    x_values$x_progress <- 'Analyzing...'
+    x_values$x_predict_table <- NULL
+    source('./lib/genre_prediction.R')
+    genre_predict(input$x_file$name)
+    x_values$x_progress <- 'Done!'
+    x_fileName <- input$x_file$name
+    x_fileName <- substring(x_fileName, 1, nchar(x_fileName)-4)
+    x_fileName <- paste0('./output/', x_fileName, '_genres.csv')
+    x_pre_genres <- read.csv(x_fileName)
+    x_values$x_predict_table <- cbind(as.matrix(c('Most likely', 'Second likely')), 
+                                      as.matrix(c(as.character(x_pre_genres[1,2]), 
+                                                  as.character(x_pre_genres[2,2]))))
+    colnames(x_values$x_predict_table) <- c('Likelyhood', 'Genre')
   })
+  
+  output$x_analyze_progess <- renderText({
+    return(x_values$x_progress)
+  })
+  
+  output$x_prediction <- renderTable({
+    return(x_values$x_predict_table)
+  })
+  
+  observeEvent(input$x_find_similar, {
+    x_values$x_similars <- NULL
+    x_fileName <- input$x_file$name
+    x_fileName <- substring(x_fileName, 1, nchar(x_fileName)-4)
+    x_fileName <- paste0('./output/', x_fileName, '_neighbors.csv')
+    x_similars <- read.csv(x_fileName)
+    if(input$x_file$name == x_similars$x[1:6]){
+      x_values$x_similars <- x_similars$x[2:7]
+    }
+    else{
+      x_values$x_similars <- x_similars$x[1:6]
+    }
+  })
+  
+  output$x_sim1_genres <- renderText({
+    x_sim1_id <- as.character(x_values$x_similars[1])
+    x_all_movie <- read.csv('./data/movie_info_all.csv')
+    x_genres <- x_all_movie[x_all_movie$id==substring(x_sim1_id,1,nchar(x_sim1_id)-4), 4]
+    return(as.character(x_genres)[1])
+  })
+  
+  output$x_sim2_genres <- renderText({
+    x_sim2_id <- as.character(x_values$x_similars[2])
+    x_all_movie <- read.csv('./data/movie_info_all.csv')
+    x_genres <- x_all_movie[x_all_movie$id==substring(x_sim2_id,1,nchar(x_sim2_id)-4), 4]
+    return(as.character(x_genres)[1])
+  })
+  
+  output$x_sim3_genres <- renderText({
+    x_sim3_id <- as.character(x_values$x_similars[3])
+    x_all_movie <- read.csv('./data/movie_info_all.csv')
+    x_genres <- x_all_movie[x_all_movie$id==substring(x_sim3_id,1,nchar(x_sim3_id)-4), 4]
+    return(as.character(x_genres)[1])
+  })
+  
+  output$x_sim4_genres <- renderText({
+    x_sim4_id <- as.character(x_values$x_similars[4])
+    x_all_movie <- read.csv('./data/movie_info_all.csv')
+    x_genres <- x_all_movie[x_all_movie$id==substring(x_sim4_id,1,nchar(x_sim4_id)-4), 4]
+    return(as.character(x_genres)[1])
+  })
+  
+  output$x_sim5_genres <- renderText({
+    x_sim5_id <- as.character(x_values$x_similars[5])
+    x_all_movie <- read.csv('./data/movie_info_all.csv')
+    x_genres <- x_all_movie[x_all_movie$id==substring(x_sim5_id,1,nchar(x_sim5_id)-4), 4]
+    return(as.character(x_genres)[1])
+  })
+  
+  output$x_sim6_genres <- renderText({
+    x_sim6_id <- as.character(x_values$x_similars[6])
+    x_all_movie <- read.csv('./data/movie_info_all.csv')
+    x_genres <- x_all_movie[x_all_movie$id==substring(x_sim6_id,1,nchar(x_sim6_id)-4), 4]
+    return(as.character(x_genres)[1])
+  })
+  
+  output$x_sim1 <-renderImage({
+    x_fileName <- paste0(x_values$x_similars[1])
+    x_fileImage <- paste0(wd , '/data/train_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=200))
+  }, deleteFile = FALSE)
+  
+  output$x_sim2 <-renderImage({
+    x_fileName <- paste0(x_values$x_similars[2])
+    x_fileImage <- paste0(wd , '/data/train_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=200))
+  }, deleteFile = FALSE)
+  
+  output$x_sim3 <-renderImage({
+    x_fileName <- paste0(x_values$x_similars[3])
+    x_fileImage <- paste0(wd , '/data/train_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=200))
+  }, deleteFile = FALSE)
+  
+  output$x_sim4 <-renderImage({
+    x_fileName <- paste0(x_values$x_similars[4])
+    x_fileImage <- paste0(wd , '/data/train_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=200))
+  }, deleteFile = FALSE)
+  
+  output$x_sim5 <-renderImage({
+    x_fileName <- paste0(x_values$x_similars[5])
+    x_fileImage <- paste0(wd , '/data/train_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=200))
+  }, deleteFile = FALSE)
+  
+  output$x_sim6 <-renderImage({
+    x_fileName <- paste0(x_values$x_similars[6])
+    x_fileImage <- paste0(wd , '/data/train_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=200))
+  }, deleteFile = FALSE)
+  
+  observeEvent(input$x_find_recommend, {
+    x_values$x_tops < NULL
+    x_all_movie <- read.csv('./data/movie_info_all.csv')
+    x_genre1 <- input$x_select_genre1
+    x_genre2 <- input$x_select_genre2
+    x_tops <- x_all_movie[intersect(grep(x_genre1, x_all_movie$genre),
+                                    grep(x_genre2, x_all_movie$genre)), ]
+    x_tops <- x_tops[order(-x_tops[,6]),]
+    x_values$x_tops <- x_tops$id[1:6]
+  })
+  
+  output$x_rec1 <-renderImage({
+    x_fileName <- paste0(x_values$x_tops[1], '.jpg')
+    x_fileImage <- paste0(wd , '/data/train_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=200))
+  }, deleteFile = FALSE)
+  
+  output$x_rec2 <-renderImage({
+    x_fileName <- paste0(x_values$x_tops[2], '.jpg')
+    x_fileImage <- paste0(wd , '/data/train_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=200))
+  }, deleteFile = FALSE)
+  
+  output$x_rec3 <-renderImage({
+    x_fileName <- paste0(x_values$x_tops[3], '.jpg')
+    x_fileImage <- paste0(wd , '/data/train_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=200))
+  }, deleteFile = FALSE)
+  
+  output$x_rec4 <-renderImage({
+    x_fileName <- paste0(x_values$x_tops[4], '.jpg')
+    x_fileImage <- paste0(wd , '/data/train_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=200))
+  }, deleteFile = FALSE)
+  
+  output$x_rec5 <-renderImage({
+    x_fileName <- paste0(x_values$x_tops[5], '.jpg')
+    x_fileImage <- paste0(wd , '/data/train_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=200))
+  }, deleteFile = FALSE)
+  
+  output$x_rec6 <-renderImage({
+    x_fileName <- paste0(x_values$x_tops[6], '.jpg')
+    x_fileImage <- paste0(wd , '/data/train_images/', x_fileName)
+    return(list(src=x_fileImage, contentType="image/jpg",alt="No Image", width=200))
+  }, deleteFile = FALSE)
+  
+})
 
 
 
--- lib/ui.R
+++ lib/ui.R
@@ -6,9 +6,40 @@ library(plotly)
 shinyUI(navbarPage(id="navbar",title="Movie Poster Designer",
                    theme="grey.css",
                    tabPanel("Introduction",
-                            titlePanel(h1("Introduction")),
-                            mainPanel(tabPanel("Introduction",includeMarkdown("introduction.md")),
-                                      img(src='contrast.jpg', align = "right"))
+                            h3("Hit Features of Movie Poster Designer:"),
+                            fluidPage( 
+                                fluidRow(
+                                    column(3, h4('1. Poster Exploration')),
+                                    column(3, h4('2. Face/Text Detection')),
+                                    column(3, h4('3. Movie Genre Predition')),
+                                    column(3, h4('4. Box Office Predition'))
+                                        ),
+                                fluidRow(
+                                  column(3, 
+                                         h5('   Learn the movie/movie poster trend in the past five years!')),
+                                  column(3, 
+                                         h5('   Is your poster easy-to-read?')),
+                                  column(3, 
+                                         h5('   How will a poster viewer perceive your poster?')),
+                                  column(3,  
+                                         h5('   Will your movie be a big success?'))
+                                ),
+                                fluidRow(
+                                  column(3, 
+                                         img(src='posters.png',
+                                             width = 220, height=350)
+                                  ),
+                                  column(3, 
+                                         img(src='face_detection.jpg',
+                                             width = 220, height=350)),
+                                  column(3, 
+                                         img(src='brain.png',
+                                             width = 220, height=350)),
+                                  column(3, 
+                                         img(src='theaters.png',
+                                             width = 220, height=350))
+                                )
+                                      )
                    ),
                    tabPanel("EDA",
                             titlePanel(h1("EDA")),
@@ -32,14 +63,97 @@ shinyUI(navbarPage(id="navbar",title="Movie Poster Designer",
                                        plotlyOutput("ggPiePlot",height="600px"))))
                             ,
                    tabPanel("Face and Text Detection",
-                            titlePanel(h1("Face Detection"))),
+                            titlePanel(h1("Face Detection"))
+                            ),
+        
+                   tabPanel("Genre Prediction",
+                            #titlePanel(h3("Genre Prediction")),
+                            sidebarPanel(fileInput('x_file', 'Choose a poster to upload',
+                                                accept = c('image/jpg', '.jpg')),
+                                         imageOutput('x_image',width = 280, height = 400),
+                                         actionButton('x_start_analyze', 'Start Analyze'),
+                                         textOutput('x_analyze_progess')),
+                            mainPanel(
+                             tabsetPanel(
+                              tabPanel('Predict the genres',
+                                       tableOutput('x_prediction')), 
+                              tabPanel('Most similar posters', 
+                                       fluidPage(
+                                         fluidRow(
+                                           column(6, h4("Find similar posters")),
+                                           column(6, actionButton('x_find_similar', 'Confirm'))
+                                         ),
+                                         fluidRow(
+                                           column(4, h5(textOutput('x_sim1_genres'))),
+                                           column(4, h5(textOutput('x_sim2_genres'))),
+                                           column(4, h5(textOutput('x_sim3_genres')))
+                                         ),
+                                         fluidRow(
+                                           column(4, imageOutput('x_sim1')),
+                                           column(4, imageOutput('x_sim2')),
+                                           column(4, imageOutput('x_sim3'))
+                                         ),
+                                         fluidRow(
+                                           column(4, h5(textOutput('x_sim4_genres'))),
+                                           column(4, h5(textOutput('x_sim5_genres'))),
+                                           column(4, h5(textOutput('x_sim6_genres')))
+                                         ),
+                                         fluidRow(
+                                           column(4, imageOutput('x_sim4')),
+                                           column(4, imageOutput('x_sim5')),
+                                           column(4, imageOutput('x_sim6'))
+                                         )
+                                         )
+                                       ),
+                              tabPanel('Box office winners',
+                                       fluidPage(
+                                         fluidRow(
+                                           fluidRow(column(12, h4("Real genres of your movie:"))),
+                                           fluidRow(
+                                             column(4, offset=1, selectInput("x_select_genre1", 
+                                                                   "Please select genre 1", 
+                                                                   choices = c("Action","Adventure","Animation","Biography","Comedy","Crime",
+                                                                               "Documentary","Drama","Family","Fantasy","Fil-Noir",
+                                                                               "History","Horror","Music","Mystery","Romance","Sci-Fi",
+                                                                               "Sport","Thriller","War","Western"), 
+                                                                   selected = 'Action', multiple = FALSE,
+                                                                   selectize = TRUE, width = NULL, size = NULL)),
+                                             column(4, offset=1, selectInput("x_select_genre2", 
+                                                                  "Please select genre 2", 
+                                                                  choices = c("Action","Adventure","Animation","Biography","Comedy","Crime",
+                                                                              "Documentary","Drama","Family","Fantasy","Fil-Noir",
+                                                                              "History","Horror","Music","Mystery","Romance","Sci-Fi",
+                                                                              "Sport","Thriller","War","Western"), 
+                                                                   selected = FALSE, multiple = FALSE,
+                                                                   selectize = TRUE, width = NULL, size = NULL)),
+                                             column(2, actionButton('x_find_recommend', 'Confirm')))
+                                         )
+                                        ),
+                                       fluidPage(
+                                         fluidRow(column(12, h4("Successful posters:"))),
+                                         fluidRow(
+                                           column(4, imageOutput('x_rec1')),
+                                           column(4, imageOutput('x_rec2')),
+                                           column(4, imageOutput('x_rec3'))
+                                         ),
+                                         fluidRow(
+                                           column(4, imageOutput('x_rec4')),
+                                           column(4, imageOutput('x_rec5')),
+                                           column(4, imageOutput('x_rec6'))
+                                         )
+                                       )
+                                       )
+                              )# end of tabset
+                             ) # end of main
+                            ), #end of tab
+                            
                    tabPanel("Box Prediction",
                             titlePanel(h1("Box Prediction"))),
                             
-                            
-                            tabPanel("Reference and Contact",
-                                     titlePanel(h2("Contact")),
-                                     mainPanel(tabPanel("Contact",includeMarkdown("contact.md"))
+                          
+                   tabPanel("Reference and Contact",
+                            titlePanel(h2("Contact")),
+                            mainPanel(tabPanel("Contact",includeMarkdown("contact.md"))
                                                           
                             ))))
 
